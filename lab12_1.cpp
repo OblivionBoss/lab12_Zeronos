53d1287c14c481c3ed5aedde801c42fa94866f4a
Zeronos <boss16805@gmail.com>
2021-12-18 19:19:51 +0700

    lab12_1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int i = 1,r;
+    string txt,grade[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    getline(cin,txt);
+    while(i<3 && getline(cin,txt)){
+       i++;
+    }
+    srand(time(0));
+    r = rand()%9;
+    cout << "You will get " << grade[r] << " in this 261102.";
+    return 0;
+}
